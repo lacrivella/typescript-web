d775fbde517ff05bb54e010675ab946cbfd01b8b
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-20 13:19:36 -0700

    created a new class called Sync that extracts the fetch and save method from User. Still gotta figure out how to fix errors around set, get, and data

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
 interface UserProps {
@@ -9,7 +8,7 @@ interface UserProps {
 
 export class User {
   public events: Eventing = new Eventing();
-  
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -19,23 +18,5 @@ export class User {
   set(update: UserProps): void {
     //whatever the second argument is, it gets copied&pasted onto the first argument
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
