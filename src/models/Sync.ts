c36517b3e74bc58cadbc1e769d731199ece59e8c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-20 13:37:44 -0700

    replaced localhost url with a constructor argument for a rootURL. imported UserProps into Sync

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosResponse } from 'axios';
+import { UserProps } from './User';
 
 export class Sync {
+  constructor(public rootURL: string) {}
+
   fetch(): void {
     axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .get(`${this.rootURL}/${this.get('id')}`)
       .then((response: AxiosResponse): void => {
         this.set(response.data);
       });
@@ -13,9 +16,9 @@ export class Sync {
     const id = this.get('id');
 
     if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
+      axios.put(`${this.rootURL}/${id}`, this.data);
     } else {
-      axios.post('http://localhost:3000/users', this.data);
+      axios.post(this.rootURL, this.data);
     }
   }
 }
