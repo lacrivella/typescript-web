ca52fc519a860b4cd2ecb9b01b4d654ce703c3ac
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-20 13:48:15 -0700

    updated save method so it returns a promise as well

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -5,16 +5,16 @@ export class Sync {
   constructor(public rootURL: string) {}
 
   fetch(id: number): AxiosPromise {
-    return axios.get(`${this.rootURL}/${id}`)
+    return axios.get(`${this.rootURL}/${id}`);
   }
 
-  save(data: UserProps): void {
+  save(data: UserProps): AxiosPromise {
     const id = data.id;
 
     if (id) {
-      axios.put(`${this.rootURL}/${id}`, data);
+      return axios.put(`${this.rootURL}/${id}`, data);
     } else {
-      axios.post(this.rootURL, data);
+      return axios.post(this.rootURL, data);
     }
   }
 }
