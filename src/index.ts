b336f6bcc4b583a20d3fc26c63d2d83157bbdb8b
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-22 14:06:13 -0700

    example of the this is javascript and why the get error is happening

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,19 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Regina', age: 16 });
 
-console.log(user.get('name'));
+// console.log(user.get('name'));
+
+// reminder on how 'this' works in javascript
+const colors = {
+  color: 'purple',
+  printColor() {
+    console.log(this.color);
+  },
+};
+
+//whatever is left to the method is the this.
+// colors.printColor();
+
+const printColor = colors.printColor;
+printColor();
+// there is nothing left of printColor() so it will be undefine
