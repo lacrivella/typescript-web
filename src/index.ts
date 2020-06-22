b00756ae112dad9806801c7254da5d58ae47f3ca
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-22 13:54:55 -0700

    added getter to the on method.

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Regina', age: 16 });
 
-// quick reminder on accessors
-class Person {
-  constructor(public firstName: string, public lastName: string) {}
-
-  get fullName(): string {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-
-const person = new Person('Aleksander', 'Tiedemann');
-console.log(person.fullName);
+user.on('change', () => {
+  console.log('time travel');
+});
