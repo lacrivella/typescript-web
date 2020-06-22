f31bfe3e4cbc1508fb0d217b7122f390cd5b5c24
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-22 13:17:59 -0700

    deleted sample code

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,5 +1,3 @@
-import { UserProps } from './User';
-
 export class Attributes<T> {
   constructor(private data: T) {}
 
@@ -12,13 +10,3 @@ export class Attributes<T> {
     Object.assign(this.data, update);
   }
 }
-
-const attrs = new Attributes<UserProps>({
-  id: 3,
-  age: 50,
-  name: 'Ulrich',
-});
-
-const name = attrs.get('name');
-const age = attrs.get('age');
-const id = attrs.get('id');
