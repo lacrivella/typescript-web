41763973aec36954e8bf6c7dd7283d4924c27a66
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-25 11:51:39 -0700

    added render method to the UserForm. Appended html element to our parent element

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,6 @@
 export class UserForm {
-  parent: Element;
-
+  constructor(public parent: Element) {}
+  
   template(): string {
     return `
       <div>
@@ -8,5 +8,12 @@ export class UserForm {
         <input />
       </div>
     `;
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.parent.append(templateElement.content);
   }
 }
