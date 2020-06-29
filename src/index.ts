a87964085f182e433cda8ddb6b1ec6f4b01b65c1
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-29 12:22:45 -0700

    users collection is rendering properly

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
 
-const user = User.buildUser({ name: 'Jonas', age: 17 });
-const root = document.getElementById('root');
+// const user = User.buildUser({ name: 'Jonas', age: 17 });
+// const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('root element cannot be found');
-}
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   throw new Error('root element cannot be found');
+// }
+
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+users.fetch();
