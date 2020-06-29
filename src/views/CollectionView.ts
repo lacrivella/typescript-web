e1350e7425f0f6566e406796d8f963cb2fae74b9
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-06-29 11:57:38 -0700

    working on render method

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -3,4 +3,13 @@ import { User } from '../models/User';
 
 export abstract class CollectionView<T, K> {
   constructor(public parent: Element, public collection: Collection<T, K>) {}
+
+  abstract renderItem(model: T, itemParent: Element): void;
+
+  render(): void {
+    //clear parent to rerender
+    this.parent.innerHTML = '';
+
+    const templateElement = document.createElement('template');
+  }
 }
